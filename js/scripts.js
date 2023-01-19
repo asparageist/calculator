89ff47c961d732380f9469f5733ca4845a3ebaf7
Joseph Murray <josephwmurray@gmail.com>
2023-01-18 20:13:22 -0800

    truncate all functions to one window.alert prompt

--- js/scripts.js
+++ js/scripts.js
@@ -14,12 +14,21 @@ function divide(number1, number2) {
   return number1 / number2;
 }
 
+
 // =================================================================
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
+const added = (add(number1,number2));
+const subtracted = (subtract(number1,number2));
+const multiplied = (multiply(number1,number2));
+const divided = (divide(number1,number2));
+const output = ("Your numbers equal " + add(number1,number2) + " when added, " + subtract(number1,number2) + " when subtracted, " + multiply(number1,number2) + " when multiplied, " + divide(number1,number2) + " when divided.");
 
-window.alert(add(number1,number2));
-window.alert(subtract(number1,number2));
-window.alert(multiply(number1,number2));
-window.alert(divide(number1,number2));
+
+window.alert(output);
+
+// window.alert(add("when added equal ",number1,number2));
+//  subtract("when subtracted equal ",number1,number2),
+//  multiply("when multiplied equal ",number1,number2),
+//  divide("when divided equal ",number1,number2));
